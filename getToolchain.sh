670744453f875412cf1f36d74b644db60b3b8db9
Michael Cullen <michael@cullen-online.com>
2019-02-06 23:52:20 +0000

    First pass at new build chain

--- getToolchain.sh
+++ getToolchain.sh
@@ -4,11 +4,14 @@ set -e
 # see packages here: http://energia.nu/packages/package_energia_index.json
 if [ "$(uname)" == "Darwin" ]; then
     CL_TOOLCHAIN_TARBALL=https://s3.amazonaws.com/energiaUS/tools/macosx/gcc-arm-none-eabi-6.3.1-20170620-mac.tar.bz2
+    DSLITE_TARBALL=https://s3.amazonaws.com/energiaUS/tools/macosx/dslite-8.2.0.1400-x86_64-apple-darwin.tar.bz2
 else
     CL_TOOLCHAIN_TARBALL=https://s3.amazonaws.com/energiaUS/tools/linux64/gcc-arm-none-eabi-6.3.1-20170620-x86_64-pc-linux-gnu.tar.bz2
+    DSLITE_TARBALL=https://s3.amazonaws.com/energiaUS/tools/linux64/dslite-8.2.0.1400-i386-x86_64-pc-linux-gnu.tar.bz2
 fi
 
 mkdir -p toolchain/Connected_Launchpad
+mkdir -p toolchain/DSLite
 mkdir -p toolchain/downloads
 
 if [ ! -f toolchain/downloads/cl_toolchain.tar.bz2 ]; then
@@ -16,10 +19,22 @@ if [ ! -f toolchain/downloads/cl_toolchain.tar.bz2 ]; then
     curl $CL_TOOLCHAIN_TARBALL --output toolchain/downloads/cl_toolchain.tar.bz2
 fi
 
+if [ ! -f toolchain/downloads/cl_dslite.tar.bz2 ]; then
+    echo "Downloading DSLite"
+    curl $DSLITE_TARBALL --output toolchain/downloads/cl_dslite.tar.bz2
+fi
+
 if [ ! -d toolchain/Connected_Launchpad/bin ]; then
-    echo "Extracting Connected Launchpad Toolchain"
     pushd toolchain/Connected_Launchpad
-    tar --strip-components=1 -xjf ../downloads/cl_toolchain.tar.bz2
+    echo "Extracting Connected Launchpad DSLite"
+    tar --strip-components=1 -xjf ../downloads/cl_dslite.tar.bz2
+    popd
+fi
+
+if [ ! -d toolchain/DSLite/bin ]; then
+    pushd toolchain/DSLite
+    echo "Extracting Connected Launchpad DSLite"
+    tar --strip-components=1 -xjf ../downloads/cl_dslite.tar.bz2
     popd
 fi
 
--- targets/tivac-launchpad/EK-TM4C1294XL.cmake
+++ targets/tivac-launchpad/EK-TM4C1294XL.cmake
@@ -5,11 +5,13 @@ set(CMAKE_MODULE_PATH  ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
 
 # Ideally this would be in the binary directory, however
 # when it runs tests it makes a mess of that
-set(TOOLCHAIN_DIR ${CMAKE_SOURCE_DIR}/toolchain/${CMAKE_SYSTEM_NAME})
+set(TOOLCHAIN_DIR ${PROJECT_SOURCE_DIR}/toolchain/${CMAKE_SYSTEM_NAME})
 
 set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/bin/arm-none-eabi-g++)
 
-include_directories(${CMAKE_SOURCE_DIR}/targets/tivac-launchpad/tivac-core/variants/EK-TM4C1294XL)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/tivac-core/variants/EK-TM4C1294XL)
 
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T ${CMAKE_SOURCE_DIR}/targets/tivac-launchpad/tivac-core/variants/EK-TM4C1294XL/lm4fcpp_snowflake.ld")
+set(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/tivac-core/variants/EK-TM4C1294XL/lm4fcpp_snowflake.ld)
+
+set(DSLITE_CONFIG ${PROJECT_SOURCE_DIR}/toolchain/DSLite/EK-TM4C1294XL.ccxml)
