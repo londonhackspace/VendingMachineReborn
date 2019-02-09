6b413cda8874457ec3c564e2a98692bd3a84ad89
Michael Cullen <michael@cullen-online.com>
2019-02-09 15:30:21 +0000

    Fixed broken build on launchpad

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,10 @@ Written to stop tgreer from throwing the vending machine away
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+                        // wait for a second
 }
